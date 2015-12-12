4f7ff34f44576ae3d0b0683da0a07a4b02e429c2
Djalma Araujo <djalma.araujo@gmail.com>
2015-12-12 01:07:46 -0300

    adjusts

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,39 +76,39 @@ $(document).ready(function() {
     }, 1000);
 
 
-    // /* ======= Google Map ======= */
-    // map = new GMaps({
-    //     div: '#map',
-    //     lat: -7.846321,
-    //     lng: -34.881915,
-    //     scrollwheel: false,
-    //     zoom: 14,
-    // });
+    /* ======= Google Map ======= */
+    map = new GMaps({
+        div: '#map',
+        lat: -7.846321,
+        lng: -34.881915,
+        scrollwheel: false,
+        zoom: 14,
+    });
+
+    map.addMarker({
+        lat: -7.846321,
+        lng: -34.881915,
+        verticalAlign: 'top',
+        title: 'Recanto das Sucupiras',
+        infoWindow: {
+            content: '<div class="note">PE-14</div><h4 class="map-title script">Acesso ao Recanto das Sucupiras</h4><div class="address"><br><span class="city-name">Estrada de Barro à esquerda</span></div>'
+        }
+    });
+
 
     // map.addMarker({
-    //     lat: -7.846321,
-    //     lng: -34.881915,
-    //     verticalAlign: 'top',
-    //     title: 'Recanto das Sucupiras',
+    //     lat: 50.969747,
+    //     lng: -3.199985,
+    //     title: 'Reception Location',
     //     infoWindow: {
-    //         content: '<div class="note">PE-14</div><h4 class="map-title script">Acesso ao Recanto das Sucupiras</h4><div class="address"><br><span class="city-name">Estrada de Barro à esquerda</span></div>'
+    //         content: '<div class="note">Reception</div><h4 class="map-title script">The Manor House</h4><div class="address"><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="city-name">City</span></div>'
     //     }
+
     // });
 
-
-    // // map.addMarker({
-    // //     lat: 50.969747,
-    // //     lng: -3.199985,
-    // //     title: 'Reception Location',
-    // //     infoWindow: {
-    // //         content: '<div class="note">Reception</div><h4 class="map-title script">The Manor House</h4><div class="address"><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="city-name">City</span></div>'
-    // //     }
-
-    // // });
-
-    // /*display marker 1 address on load */
-    // google.maps.event.trigger(map.markers[0], 'click');
-    // /*display marker 2 address on load */
+    /*display marker 1 address on load */
+    google.maps.event.trigger(map.markers[0], 'click');
+    /*display marker 2 address on load */
     // google.maps.event.trigger(map.markers[1], 'click');
 
 
